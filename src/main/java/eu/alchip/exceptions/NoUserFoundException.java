a4bd1a3a66be03df7c01e0408dda8e3b71dd879e
Popa Alexandru Ciprian <alexandrupopa@alice.it>
2019-02-27 10:39:16 +0100

    Aggiunta Exception per utente non trovato al login

---
+++ src/main/java/eu/alchip/exceptions/NoUserFoundException.java
@@ -0,0 +1,10 @@
+package eu.alchip.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND,
+        reason="User Not Found")
+public class NoUserFoundException extends RuntimeException {
+
+}
