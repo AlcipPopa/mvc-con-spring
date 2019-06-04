3970c88c4dc9db6c9b8bb6196755aeca10d6687f
Popa Alexandru Ciprian <alexandrupopa@alice.it>
2019-06-04 13:13:32 +0200

    Update HibernateConfig.java

--- src/main/java/eu/alchip/config/HibernateConfig.java
+++ src/main/java/eu/alchip/config/HibernateConfig.java
@@ -30,8 +30,8 @@ public class HibernateConfig {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
         dataSource.setUrl("jdbc:postgresql://localhost:5432/databaseprova");
-        dataSource.setUsername("iragogeta");
-        dataSource.setPassword("1samurai");
+        dataSource.setUsername("USERNAME_QUI");
+        dataSource.setPassword("PASSWORD_QUI");
 
         return dataSource;
     }
